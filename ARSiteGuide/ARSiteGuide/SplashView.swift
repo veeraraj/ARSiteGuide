04cd0da57009e9f721420fa47f66e44a0c96bfbf
Veeraraj <veeraraj.v@gmail.com>
2023-05-09 11:51:15 +0200

    [0003] updated splash screen

--- ARSiteGuide/ARSiteGuide/SplashView.swift
+++ ARSiteGuide/ARSiteGuide/SplashView.swift
@@ -8,20 +8,34 @@
 import SwiftUI
 
 struct SplashView: View {
+
+    @State var isSplashDone: Bool = false
+
     var body: some View {
         VStack {
-            Image("WKLogo")
-                .resizable()
-                .frame(maxWidth: 300, maxHeight: 300, alignment: .center)
+            if isSplashDone {
+                LoginView()
+            } else {
+                Image("WKLogo")
+                    .resizable()
+                    .frame(maxWidth: 300, maxHeight: 300, alignment: .center)
 
-            Text("Virtual Site Guide")
-                .font(.largeTitle)
-                .foregroundStyle(RadialGradient(
-                    colors: [.red, .green, .blue],
-                    center: .center,
-                    startRadius: 0,
-                    endRadius: 100))
-                .padding(.top, 96)
+                Text("Virtual Site Guide")
+                    .font(.largeTitle)
+                    .foregroundStyle(RadialGradient(
+                        colors: [.red, .green, .blue],
+                        center: .center,
+                        startRadius: 0,
+                        endRadius: 100))
+                    .padding(.top, 96)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isSplashDone = true
+                }
+            }
         }
     }
 }
