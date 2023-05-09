7d50867ec0cedafd678d953f1290cd5d33cba522
Veeraraj <veeraraj.v@gmail.com>
2023-05-09 10:53:21 +0200

    [001] Base project created

---
+++ ARSiteGuide/ARSiteGuide/ARSiteGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARSiteGuideApp.swift
+//  ARSiteGuide
+//
+//  Created by Veeraraj on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ARSiteGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
